ec53464682705f489e753152eda8578d8007e87b
skylock <emanuil_usa@yahoo.com>
2015-09-18 12:32:01 +0300

    first commit for the second problem

---
+++ ExcelColumns.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimitiveTypes
+{
+    [TestClass]
+    public class ExcelColumns
+    {
+        [TestMethod]
+        [TestCategory("02_ExcelColumns")]
+        public void Compute_Combination_For_Column_Number_Less_Than_27 () {
+            int columnNumber = 2;
+
+            string columnID = GetColumnID(columnNumber);
+
+            Assert.AreEqual("B", columnID);
+
+        }
+
+        [TestMethod]
+        [TestCategory("02_ExcelColumns")]
+        public void Compute_Combination_For_Column_Number_Equal_To_27() {
+            int columnNumber = 27;
+
+            string columnID = GetColumnID(columnNumber);
+
+            Assert.AreEqual("AA", columnID);
+
+        }
+
+        private string GetColumnID(int columnNumber) {
+            string result = string.Empty;
+            return result + GetChar(columnNumber);
+        }
+
+        private static char GetChar(int index) {
+            return (char)(('A' - 1) + index);
+        }
+    }
+}
