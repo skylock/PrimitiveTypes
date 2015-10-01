810ff23f5266b401fa7d3485b27615e4d16f2352
skylock <emanuil_usa@yahoo.com>
2015-10-02 01:29:56 +0300

    added BankLoan  ( needs more work )

---
+++ BankLoan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimitiveTypes
+{
+    [TestClass]
+    public class BankLoan
+    {
+        [TestMethod]
+        public void Compute_Rate_For_First_Month() {
+
+            decimal credit = 20000;
+                int months = 60;
+            double intrest = 16.04;
+
+            double payment = ComputePayment(credit, months, intrest);
+
+            Assert.AreEqual(0, payment);
+        }
+
+        private double ComputePayment(decimal credit, int months, double intrest) {
+
+        }
+    }
+}
