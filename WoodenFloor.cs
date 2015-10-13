a28d6a18a1478e3cf54c7df05a8d2ca97e9a35e0
skylock <emanuil_usa@yahoo.com>
2015-10-13 23:28:28 +0300

    [10] final solution to Wooden Floor

--- WoodenFloor.cs
+++ WoodenFloor.cs
@@ -6,30 +6,112 @@ namespace PrimitiveTypes
     [TestClass]
     public class WoodenFloor
     {
+        public bool lossesExist = false;
+
         [TestMethod]
-        public void Compute_Boards_Needed_When_BoardSide_Is_Exact_Multiple_For_Floor_Length_Or_Width() {
-            Tuple<uint, uint> boardSizeInMilimeters = new Tuple<uint, uint>(300, 700);
-            Tuple<uint, uint> roomSizeInMilimeters = new Tuple<uint, uint>(3000, 7000);
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_Ll_Input_Values_Are_Zero()
+        {
+
+            Size boardSize = new Size(0, 0);
+            Size roomSize = new Size(0, 0);
+            uint lossesPercentage = 0;
+
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
+
+            Assert.AreEqual<uint>(0, neededBoards);
+        }
+
+        [TestMethod]
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_Losses_Percentage_Is_Zero()
+        {
+
+            Size boardSize = new Size(300, 700);
+            Size roomSize = new Size(3000, 7000);
+            uint lossesPercentage = 0;
+
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
+
+            Assert.AreEqual<uint>(100, neededBoards);
+        }
+
+        [TestMethod]
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_BoardSide_Length_Or_Width_Is_Zero()
+        {
+
+            Size boardSize = new Size(0, 10);
+            Size roomSize = new Size(3000, 7000);
             uint lossesPercentage = 15;
 
-            double neededBoards = ComputeNeededBoards(roomSizeInMilimeters, boardSizeInMilimeters, lossesPercentage);
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
 
-            Assert.AreEqual(115, neededBoards);
+            Assert.AreEqual<uint>(0, neededBoards);
         }
 
-        private uint ComputeNeededBoards(Tuple<uint, uint> roomSize, Tuple<uint, uint> boardSize, uint lossesPercentage) {
-            uint boardsOnWidth = ComputeNeededBoardsOnSide(roomSize.Item1, boardSize.Item1);
-            uint boardsOnLength = ComputeNeededBoardsOnSide(roomSize.Item2, boardSize.Item2);
-            uint boards =  boardsOnWidth * boardsOnLength;
-            return (uint)GetLosses(boards, lossesPercentage) + 1;
+        [TestMethod]
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_RoomSide_Length_Or_Width_Is_Zero()
+        {
+
+            Size boardSize = new Size(300, 700);
+            Size roomSize = new Size(0, 7000);
+            uint lossesPercentage = 15;
+
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
+
+            Assert.AreEqual<uint>(0, neededBoards);
         }
 
-        private double GetLosses(uint number, uint percent) {
-            double percentage = percent / 100.0;
-            return (double)number * (percentage + 1);
+        [TestMethod]
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_BoardSide_Length_Or_Width_Is_Smaller_ThanRoom_Size()
+        {
+
+            Size boardSize = new Size(300, 700);
+            Size roomSize = new Size(30, 70);
+            uint lossesPercentage = 15;
+
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
+
+            Assert.AreEqual<uint>(1, neededBoards);
         }
 
-        private static uint ComputeNeededBoardsOnSide(uint sideSize, uint boardSide) {
+        [TestMethod]
+        [TestCategory("10_Wooden_Floor")]
+        public void Compute_Boards_Needed_When_BoardSide_Is_Exact_Multiple_For_Floor_Length_Or_Width() {
+
+            Size boardSize = new Size(300,700);
+            Size roomSize = new Size(3000, 7000);
+            uint lossesPercentage = 15;
+
+            uint neededBoards = ComputeNeededBoards(roomSize, boardSize, lossesPercentage);
+
+            Assert.AreEqual<uint>(100, neededBoards);
+        }
+
+
+        private uint ComputeNeededBoards(Size roomSize, Size boardSize, uint lossesPercentage) {
+            uint boards = GetNeededBoards(roomSize, boardSize);
+            uint boardsToCoverLosses = GetLosses(boards, lossesPercentage);
+            return boards + boardsToCoverLosses;
+        }
+
+        private uint GetLosses(uint boards, uint lossesPercentage) {
+            if (!lossesExist) return 0;
+            return (uint)(boards * (lossesPercentage / 100.0));
+        }
+
+        private uint GetNeededBoards(Size roomSize, Size boardSize)
+        {
+            uint boardsOnWidth = GetNeededBoardsOnSide(roomSize.length, boardSize.length);
+            uint boardsOnLength = GetNeededBoardsOnSide(roomSize.width, boardSize.width);
+            return boardsOnWidth * boardsOnLength;
+
+        }
+
+        private uint GetNeededBoardsOnSide(uint sideSize, uint boardSide) {
             if (sideSize < boardSide && sideSize > 0) {
                 return 1;
             } else {
@@ -39,12 +121,35 @@ namespace PrimitiveTypes
                     if (sideSize % boardSide == 0) {
                         return sideSize / boardSide;
                     } else {
+                        lossesExist = true;
                         return (sideSize / boardSide) + 1;
                     }
                 } else {
                     return 0;
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct Size 
+    {
+        /// <summary>
+        /// The board length
+        /// </summary>
+        public uint length, width;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Board"/> struct.
+        /// </summary>
+        /// <param name="length">form length in milimeters.</param>
+        /// <param name="width">Form width in milimeters.</param>
+        public Size(uint length, uint width)
+        {
+            this.length = length;
+            this.width = width;
         }
     }
 }
