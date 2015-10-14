81ebc5f4729bffd6266e69f325fcafa026acfac4
skylock <emanuil_usa@yahoo.com>
2015-10-15 00:25:01 +0300

    [12] Secret Messages - work in progress on encrypting message

--- SecretMessages.cs
+++ SecretMessages.cs
@@ -11,14 +11,34 @@ namespace PrimitiveTypes
         public void Test_Encription_When_Message_Is_Not_Empty() {
             int key = 4;
             string message = "nicaieri nu e ca acasa";
-            string expected = "neeaircsciaaanaxiucw";
-            string actual = EncryptMessage(message, key);
+            string [,] expectedMessage = new string [,]
+            {
+                {"n", "e", "e", "a"},
+                {"i", "r", "c", "s"},
+                {"c", "i", "a", "a"},
+                {"a", "n", "a", "x"},
+                {"i", "u", "c", "w"}
+            };
 
-            Assert.AreEqual(expected, actual);
+            string actualMessage = EncryptMessage(message, key);
+
+            Assert.AreEqual(expectedMessage, actualMessage);
         }
 
-        private string EncryptMessage(string message, int key) {
-            throw new NotImplementedException();
+        private string EncryptMessage(string message, int columns) {
+            int rows = message.Length / columns;
+            char[,] encryptedMessage = new char[rows, columns];
+            int charIndex = 0;
+            for (int i = 0; i < columns; i++)
+            {
+                for(int j = 0; j < rows; j++)
+                {
+                    char ch = message[charIndex];
+                    encryptedMessage[j, i] = ch;
+                    charIndex++;
+                }
+            }
+            return string.Empty;
         }
     }
 }
