1c3888074b9be519879fcb7a33c6259f6834b96c
skylock <emanuil_usa@yahoo.com>
2015-10-14 15:15:13 +0300

    [12] Secret Messages - Initial commit

---
+++ SecretMessages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimitiveTypes
+{
+    [TestClass]
+    public class SecretMessages
+    {
+        [TestMethod]
+        [TestCategory("12_Secret_Messages")]
+        public void Test_Encription_When_Message_Is_Not_Empty() {
+            int key = 4;
+            string message = "nicaieri nu e ca acasa";
+            string expected = "neeaircsciaaanaxiucw";
+            string actual = EncryptMessage(message, key);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private string EncryptMessage(string message, int key) {
+            throw new NotImplementedException();
+        }
+    }
+}
