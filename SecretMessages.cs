10a07424f096b057979fc0e49fd6be2bc23da411
skylock <emanuil_usa@yahoo.com>
2015-10-16 12:55:55 +0300

    [12] new encryption algorithm

--- SecretMessages.cs
+++ SecretMessages.cs
@@ -7,6 +7,7 @@ namespace PrimitiveTypes
     [TestClass]
     public class SecretMessages
     {
+
         [TestMethod]
         [TestCategory("12_Secret_Messages")]
         public void Test_Encription_When_Message_Is_Not_Empty() {
@@ -20,48 +21,23 @@ namespace PrimitiveTypes
         }
 
         private string EncryptMessage(string message, int columns) {
-            string cleandStr = CleanString(message);
-            int rows = GetRows(cleandStr, columns);
-            int length = (rows * columns) - cleandStr.Length;
-            string preparedStr = cleandStr + GetRandomStr(length);
-            string [] matrix = CreateSubstrings(preparedStr, columns);
-            string[] columnToRow = new string[rows];
-            for (int i = 0; i <= columns; i++) {
-                columnToRow[i] = ReadMatrixColumn(matrix, i);
-            }
-            string encryptedStr = string.Join("",columnToRow);
-            return encryptedStr;
-        }
-
-        private string ReadMatrixColumn(string[] matrix, int column) {
-            char[] result = new char[matrix.Length];
-            for (int i = 0; i < matrix.Length; i++) {
-                result[i] = matrix[i][column];
-            }
-            return new string(result);
-        }
-
-        private static int GetRows(string cleandStr, int columns) {
-            int rows = (int)Math.Ceiling((double)cleandStr.Length / columns);
-            return rows;
-        }
-
-        private string[] CreateSubstrings(string preparedStr, int size) {
-            int rows = preparedStr.Length / size;
-            string[] subStrings = new string[size];
-            for (int i = 0; i < subStrings.Length; i++) {
-                subStrings[i] = preparedStr.Substring(i * rows, rows);
-            }
-            return subStrings;
-        }
-
-        private string GetRandomStr(int length) {
             Random rgen = new Random(7);
-            char[] randomStr = new char[length];
-            for (int i = 0; i < randomStr.Length; i++ ) {
-                randomStr[i] = GetRandomChar(rgen);
+            char[] cleanedString = CleanString(message).ToCharArray();
+            int rows = (int)Math.Ceiling((double)cleanedString.Length / columns);
+            char[] encryptedMessage = new char[rows * columns];
+            int i = 0;
+            int k = 0;
+            int ci = 0;
+            while (ci < cleanedString.Length) {
+                for (i = k; i < encryptedMessage.Length; i += columns) {
+                    char value = ci >= cleanedString.Length ? GetRandomChar(rgen) : cleanedString[ci];
+                    encryptedMessage[i] = value;
+                    ci++;
+                }
+                k++;
             }
-            return new string(randomStr);
+            string result = new string(encryptedMessage);
+            return result;
         }
 
         private char GetRandomChar(Random rgen) {
