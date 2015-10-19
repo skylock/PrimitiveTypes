0f33a87f068e53d537c85d8aacaf446c1cae5465
skylock <emanuil_usa@yahoo.com>
2015-10-19 13:58:54 +0300

    [13] Started working on base convert

---
+++ BaseConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace PrimitiveTypes
+{
+    [TestClass]
+    public class BaseConversion
+    {
+        [TestMethod]
+        [TestCategory("13_Convert_To_Base")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Convert_To_Base_2_No_9() {
+            uint value = 9;
+            uint  _base = 2;
+            string expected = Convert.ToString(value, 2);
+            
+            string result = ConvertToBase(value, _base);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        private string ConvertToBase(uint value, uint _base) {
+            return string.Empty;
+        }
+    }
+}
