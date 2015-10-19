d41ef0c1e0322b1cc19c1c95f5b3d4de304b5c89
skylock <emanuil_usa@yahoo.com>
2015-10-19 15:13:14 +0300

    [13] convert to any base done

--- BaseConversion.cs
+++ BaseConversion.cs
@@ -10,18 +10,47 @@ namespace PrimitiveTypes
         [TestMethod]
         [TestCategory("13_Convert_To_Base")]
         [ExpectedException(typeof(ArgumentException))]
+        public void Convert_To_A_Base_Lower_Than_2() {
+            string result = ConvertToBase(9, 0);
+        }
+        [TestMethod]
+        [TestCategory("13_Convert_To_Base")]
         public void Convert_To_Base_2_No_9() {
-            uint value = 9;
-            uint  _base = 2;
-            string expected = Convert.ToString(value, 2);
+            string expected = Convert.ToString(9, 2);
             
-            string result = ConvertToBase(value, _base);
+            string result = ConvertToBase(9,2);
 
             Assert.AreEqual(expected, result);
         }
 
-        private string ConvertToBase(uint value, uint _base) {
-            return string.Empty;
+        [TestMethod]
+        [TestCategory("13_Convert_To_Base")]
+        public void Convert_To_Base_2_No_172() {
+            string expected = Convert.ToString(172, 2);
+
+            string result = ConvertToBase(172, 2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        /// <summary>
+        /// Converts a number from base 10 to any base.
+        /// </summary>
+        /// <param name="value">The value to be converted.</param>
+        /// <param name="toBase">The output base.</param>
+        /// <returns></returns>
+        private string ConvertToBase(int value, int toBase) {
+            if (toBase < 2) throw new ArgumentException("Invalid base.");
+            string result = string.Empty;
+            int divisionResult = value;
+            int remainder;
+
+            while (divisionResult > 0) {
+                remainder = divisionResult  % toBase;
+                result = remainder + result;
+                divisionResult = (divisionResult - remainder) / toBase;
+            }
+            return result;
         }
     }
 }
