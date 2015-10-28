a55592aa9476fc75b4973a960898600dc734e58c
skylock <emanuil_usa@yahoo.com>
2015-10-28 11:57:17 +0200

    [14] implemented method to add multiple readings

--- Meteo.cs
+++ Meteo.cs
@@ -18,17 +18,35 @@ namespace PrimitiveTypes
 
             Assert.AreEqual(21.73, reading);
         }
+
+        [TestMethod]
+        [TestCategory("14_Weather_Station")]
+        public void Test_Add_A_List_Of_Daily_Readings() {
+            var readings = new List<double> { -5, -10, 0.23, 12.53, 17.05 };
+            
+            WeatherStation weatherStation = new WeatherStation();
+
+            weatherStation.AddReading(readings);
+
+            double reading = weatherStation.GetReading(4);
+
+            Assert.AreEqual(17.05, reading);
+        }
     }
 
     public struct WeatherStation 
     {
         private static List<double> dailyReadings = new List<double>();
 
-        internal void AddReading(double temperature) {
+        public void AddReading(double temperature) {
             dailyReadings.Add(temperature);
         }
 
-        internal double GetReading(uint day) {
+        public void AddReading(List<double> temperatures) {
+            foreach (var value in temperatures) dailyReadings.Add(value);
+        }
+
+        public double GetReading(uint day) {
             ValidateInput(day);
             return dailyReadings[(int)day];
         }
