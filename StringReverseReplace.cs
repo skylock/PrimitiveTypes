9ec55ca5f682f9545653a4c2111081c29f91dc69
skylock <emanuil_usa@yahoo.com>
2015-10-30 16:12:55 +0200

    [15] added test cases when searched character is not found and inserted string is null or empty

--- StringReverseReplace.cs
+++ StringReverseReplace.cs
@@ -59,17 +59,42 @@ namespace PrimitiveTypes
 
             Assert.AreEqual("a###ca###ca###ca###ca###c", actual);
         }
+        
+        [TestMethod]
+        [TestCategory("15_String_Reverse_And_Replace")]
+        public void ReplaceInString_SearchedCharacterIsNotFound_ReturnsString() {
+            string value = "abcabcabcabcabc";
 
-        private string ReplaceInString(string value, char ch, string inserted)
+            string actual = ReplaceInString(value, 'd', "###");
+
+            Assert.AreEqual("abcabcabcabcabc", actual);
+        }
+
+        [TestMethod]
+        [TestCategory("15_String_Reverse_And_Replace")]
+        public void ReplaceInString_ReplaceStringIsEmpty_ReturnsString() {
+            string value = "abcabcabcabcabc";
+
+            string actual = ReplaceInString(value, 'b', string.Empty);
+
+            Assert.AreEqual("abcabcabcabcabc", actual);
+        }
+
+        private string ReplaceInString(string value, char ch, string insertion)
         {
             if (string.IsNullOrEmpty(value))
             {
                 return value;
             }
+
+            if (string.IsNullOrEmpty(insertion)) {
+                return value;
+            }
+
             char car = value[0];
             string cdr = value.Substring(1);
-            if (car == ch) return inserted + ReplaceInString(cdr, ch, inserted);
-            return car + ReplaceInString(cdr, ch, inserted);
+            if (car == ch) return insertion + ReplaceInString(cdr, ch, insertion);
+            return car + ReplaceInString(cdr, ch, insertion);
         }
 
         private string ReverseString(string value)
