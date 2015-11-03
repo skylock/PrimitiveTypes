d23d841dd0af7d01370a0c6c9b3599fa3e0cd390
skylock <emanuil_usa@yahoo.com>
2015-11-03 14:22:34 +0200

    [16] Started work on pocket calculator

---
+++ PocketCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimitiveTypes
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PocketCalculator
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsResult() {
+            string input = "+ 3 4";
+
+            double result = Compute(input);
+
+            Assert.AreEqual(7, result, 0);
+        }
+
+        private double Compute(string input) {
+            throw new NotImplementedException();
+        }
+    }
+}
